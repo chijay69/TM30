f4f747a2c29c6ddd9c74c8c40adbfc31d4746ad7
alozie chijindu <chijay69@outlook.com>
2025-02-26 18:10:37 +0100

    enabled eureka service

--- src/main/java/com/example/TM30/order/repository/OrderRepository.java
+++ src/main/java/com/example/TM30/order/repository/OrderRepository.java
@@ -1,7 +1,11 @@
 package com.example.TM30.order.repository;
 
 import com.example.TM30.order.entity.Order;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
+@Transactional
 public interface OrderRepository extends JpaRepository<Order, Long> {
 }
